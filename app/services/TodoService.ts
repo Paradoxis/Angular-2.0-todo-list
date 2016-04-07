38980b01f7cd97367e1006387e9e9049ae5d2e6e
Paradoxis <luke@paradoxis.nl>
2016-04-07 17:37:49 +0200

    Removed unused code

--- app/services/TodoService.ts
+++ app/services/TodoService.ts
@@ -4,25 +4,6 @@ export class TodoService
     {
         return [];
     }
-    
-    public authenticate() : boolean 
-    {
-        try {
-            this.validateHasUsername();
-            this.validateHasPassword();
-            this.validateCredentials();
-            return true;
-        } catch (e) {
-            return false;
-        }
-    }
-    
-    public validateHasUsername() : void 
-    {
-        if (this.username == "") {
-            throw new Error("No username");
-        }
-    }
 }
 
 export class TodoItem
